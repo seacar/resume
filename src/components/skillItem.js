c4baa18a9c77f25eb9760cf5e3b9f4d809808aed
seacar <theseancarroll@gmail.com>
2020-09-21 16:36:20 -0500

    add skill items

---
+++ src/components/skillItem.js
@@ -0,0 +1,22 @@
+import React from "react"
+import styled from "@emotion/styled"
+import Fade from 'react-reveal/Fade'
+
+const StyledSkillItemContainer = styled.div `
+  margin-bottom: 3%;
+`
+
+const StyledSkillItemTitle = styled.h2 `
+  color: #663399;
+`
+
+const SkillItem = ({ skillItemTitle, skillItemYears }) => (
+    <Fade bottom>
+        <StyledSkillItemContainer>
+            <StyledSkillItemTitle>{skillItemTitle}</StyledSkillItemTitle>
+            <StyledSkillItemYears>{skillItemYears}</StyledSkillItemYears>
+        </StyledSkillItemContainer>
+    </Fade>
+)
+
+export default SkillItem
