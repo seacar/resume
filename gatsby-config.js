af7e87c81c1c9b2ba30d6e45341452fdc625940f
seacar <theseancarroll@gmail.com>
2020-09-21 11:01:38 -0500

    adds react google fonts plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Sean Patrick Carroll`,
+    description: `Personal resume site built using Gatsby with many react based plugins`,
     author: `@gatsbyjs`,
   },
   plugins: [
@@ -32,6 +32,16 @@ module.exports = {
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
+    },
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `limelight`,
+          `source sans pro\:300,400,400i,700` // you can also specify font weights and styles
+        ],
+        display: 'swap'
+      }
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
