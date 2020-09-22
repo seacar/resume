897402d782daeb30f3eeac6e99f35e57f316e395
seacar <theseancarroll@gmail.com>
2020-09-21 22:33:55 -0500

    moves to bottom

--- src/components/socials.js
+++ src/components/socials.js
@@ -8,7 +8,7 @@ import { faFilePdf } from '@fortawesome/free-solid-svg-icons'
 const SidebarContactContainer = styled.div `
   margin-bottom: 3%;
   position: absolute;
-  top: 0vh;
+  bottom: 0vh;
   left: 0;
   width: 20%;
   padding: 2%;
