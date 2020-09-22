cf86acf629716f1c8835aa5298489f6926da360b
seacar <theseancarroll@gmail.com>
2020-09-21 21:34:09 -0500

    updates mobile styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -46,7 +46,7 @@ const IndexPage = () => (
         <SectionItem sectionItemTitle="ADK Group" sectionItemYears="September 2019 - August 2020" sectionItemHighlight="Drupal Developer" sectionItemDescription="After leaving 3BL Media I began at ADK Group, a world class digital agency, as a Drupal Developer.  In this role I have had the opportunity to work on a number of different websites mostly utilizing Drupal 8." />
         <SectionItem sectionItemTitle="3BL Media" sectionItemYears="January 2019 - August 2019" sectionItemHighlight="Lead Devops Engineer" />
         <SectionItem sectionItemYears="February 2017 - January 2019" sectionItemHighlight="Web Developer" sectionItemDescription="Upon graduation from Georgia Tech I transistioned to 3BL Media as a full time Full-Stack Web Developer working on a number of different web properties owned by 3BL.  I worked in a number of different tech stacks including Drupal 7, Drupal 8, EmberJS, NodeJS, and ReactJS." />
-        <SectionItem sectionItemTitle="Georgia Insitute of Technology" sectionItemYears="August 2012 - August 2016" sectionItemHighlight="IT Student Assistant" sectionItemDescription="While enrolled at Georgia Tech I worked part time for the Office of International Education as an IT Student Assistant.  In this role I contributed to the development and maintinence of the department website (Drupals 6 and 7) and provided desktop IT support to the office of 35+ full time employees." />
+        <SectionItem sectionItemTitle="Georgia Institute of Technology" sectionItemYears="August 2012 - August 2016" sectionItemHighlight="IT Student Assistant" sectionItemDescription="While enrolled at Georgia Tech I worked part time for the Office of International Education as an IT Student Assistant.  In this role I contributed to the development and maintinence of the department website (Drupals 6 and 7) and provided desktop IT support to the office of 35+ full time employees." />
       </SectionInfoInner>
     </SectionInfo>
     <Section sectionTitle="Skills" />
@@ -61,7 +61,7 @@ const IndexPage = () => (
       <SkillItem skillItemTitle="Pantheon" skillItemYear="2018" />
       <SkillItem skillItemTitle="AWS" skillItemYear="2018" />
       <SkillItem skillItemTitle="Docker" skillItemYear="2018" />
-      <SkillItem skillItemTitle="Lando" skillItemYear="2019" />
+      <SkillItem skillItemTitle="Lando" skillItemYear="2018" />
     </SectionInfoInner>
     <Section sectionTitle="Hobbies" />
     <SectionInfoInner>
