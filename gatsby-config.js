9615bda92ec630a5891222840c07deb99ec3d901
seacar <theseancarroll@gmail.com>
2020-09-25 00:51:21 -0500

    adds font awesome css prerendering

--- gatsby-config.js
+++ gatsby-config.js
@@ -45,7 +45,8 @@ module.exports = {
         display: 'swap'
       }
     },
-    `gatsby-plugin-build-date`
+    `gatsby-plugin-build-date`,
+    `gatsby-plugin-fontawesome-css`
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
